0509292225271e5393aceda417d4d9b9c14abcd0
1337kid <45532566+1337kid@users.noreply.github.com>
2024-09-21 15:51:39 +0530

    navbar component & navitems

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+"use client";
+import logo from '@/assets/debutsav.svg';
+import { useState } from "react";
+import Image from "next/image";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
+import { NavItems } from '@/constants';
+import Link from 'next/link';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [mobilenav, setmobilenav] = useState(false);
 
-export default Navbar
+  const handleHamburger = () => {
+    setmobilenav(!mobilenav);
+  };
+
+  return (
+    <div
+      className='w-screen h-[70px] flex flex-row items-center justify-between px-6 lg:px-0 lg:justify-around py-3 fixed top-0 z-[2] bg-zinc-900'
+    >
+      <a href='/'>
+        <Image  src={logo} alt="true" className="h-[50px] -mr-[30px]" />
+      </a>
+      <button
+        onClick={handleHamburger}
+        className="text-4xl block lg:hidden mr-3"
+      >
+        {!mobilenav ? (
+          <RxHamburgerMenu className="text-[40px] text-gray-light"/>
+        ) :           
+          <IoCloseOutline className="text-[40px] text-gray-light"/>}
+      </button>
+      <div
+        className={`text-gray-light transition-all duration-300 ease-in-out font-medium lg:static lg:w-max lg:h-max lg:pt-0 lg:bg-transparent lg:block lg:mr-3 fixed top-0 w-screen pt-24 h-screen z-[-1] text-center  items-center ${
+          mobilenav
+            ? "right-0 flex flex-col gap-8 bg-zinc-900  "
+            : "right-full flex flex-col gap-8 text-opacity-0  "
+        }`}
+      >
+        {NavItems.map((item,index) => (
+          <Link
+            key={index}  
+            href={item.link}
+            className="group text-gray-light transition-all duration-300 ease-in-out lg:ml-8"
+            onClick={handleHamburger}
+          >
+            <span className="bg-left-bottom text-xl font-normal bg-gradient-to-r from-red to-red bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration ease-out">
+              {item.title}
+            </span>
+          </Link>
+        ))}
+        <button className="lg:hidden block  register text-xl text-gray-light bg-red px-10 py-2">
+          <a href={"/Register"}>Register</a>
+        </button>
+      </div>
+      <button className="lg:block hidden register text-xl text-gray-light bg-red px-10 py-2">
+        <a href={"/Register"}>Register</a>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ constants/index.js
@@ -0,0 +1,26 @@
+export const NavItems = [
+    {
+        title: "Home",
+        link: "/"
+    },
+    {
+        title: "About",
+        link: "/#about"
+    },
+    {
+        title: "Speakers",
+        link: "/#speakers"
+    },
+    {
+        title: "Sponsors",
+        link: "/#sponsors"
+    },
+    {
+        title: "Become A Sponsor",
+        link: "/becomeasponsor"
+    },
+    {
+        title: "Contacts",
+        link: "/#Contacts"
+    }
+]
