def4a619aba9176ae01a127104d8bb281e480bd8
1337kid <45532566+1337kid@users.noreply.github.com>
2024-09-22 19:24:33 +0530

    setup speakers & sponsors section & remove it from navbar

--- components/Sponsors.jsx
+++ components/Sponsors.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
 const Sponsors = () => {
-  return (
-    <div id="sponsors">Sponsors</div>
-  )
-}
+  const platinumSponsors = [
+  ];
+  const goldSponsors = [
+  ];
+  const silverSponsors = [
+  ];
 
-export default Sponsors
+  const renderSponsors = (sponsors, width) =>
+    sponsors.map((sponsor) => (
+      <div className="p-4 md:p-10 md:mx-10">
+        <img src={sponsor} width={width} alt="" srcset="" />
+      </div>
+    ));
+
+  return (
+    <div className="max-w-screen-xl flex flex-col px-4 py-8 mx-auto lg:gap-8 ">
+      <h1 className="text-4xl font-bold text-center mb-20">Sponsors</h1>
+      <div className="flex flex-col justify-center items-center gap-16">
+        <h2 className="text-2xl font-semibold">Platinum</h2>
+        <div className="flex flex-col justify-center items-center ">
+          {renderSponsors(platinumSponsors,400)}
+        </div>
+        <h2 className="text-2xl font-semibold">Gold</h2>
+        <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+          {renderSponsors(goldSponsors,300)}
+        </div>
+        <h2 className="text-2xl font-semibold">Silver</h2>
+        <div className="grid grid-cols-1  md:grid-cols-2   ">
+          {renderSponsors(silverSponsors,200)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sponsors;
--- constants/index.js
+++ constants/index.js
@@ -7,14 +7,14 @@ export const NavItems = [
         title: "About",
         link: "/#about"
     },
-    {
-        title: "Speakers",
-        link: "/#speakers"
-    },
-    {
-        title: "Sponsors",
-        link: "/#sponsors"
-    },
+    // {
+    //     title: "Speakers",
+    //     link: "/#speakers"
+    // },
+    // {
+    //     title: "Sponsors",
+    //     link: "/#sponsors"
+    // },
     {
         title: "Become A Sponsor",
         link: "/becomeasponsor"
@@ -113,4 +113,37 @@ export const BecomeSponsorRowData = [
       startColumn: 5
       
     }
+]
+
+export const speakersList = [
+  {
+    name: "Andrew Bastin",
+    designation: "CTO, Hoppscotch",
+    img: "https://cloud.disroot.org/apps/files_sharing/publicpreview/tkPPNJbggKnygor?file=/&fileId=256717046&x=3360&y=2100&a=true",
+    url: "https://andrewbast.in/"
+  },
+  {
+    name: "Athul Cyriac Ajay",
+    designation: "Platform Engineer, Frappe",
+    img: "https://avatars.githubusercontent.com/u/40897573?v=4",
+    url: "https://athulcyriac.in/"
+  },
+  {
+    name: "Sahil Dhiman",
+    designation: "Debian Developer",
+    img: "https://cloud.disroot.org/apps/files_sharing/publicpreview/fFmgrDfHzWNBN8R?file=/&fileId=256716670&x=3360&y=2100&a=true",
+    url: "https://blog.sahilister.in/"
+  },
+  {
+    name: "Kurian Benoy",
+    designation: "Volunteer, SMC",
+    img: "https://kurianbenoy.com/posts/images/fossasia_summit_2019/my_lighting_talk.jpg",
+    url: "https://kurianbenoy.com"
+  },
+  {
+    name: "Abraham Raji",
+    designation: "Debian Developer",
+    img: "https://avatars.githubusercontent.com/u/32333507?v=4",
+    url: "https://abrahamraji.in"
+  },
 ]
