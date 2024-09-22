4d0da53cc657a8a95941212bde2081cfc8706ac5
aaroongeorge2005 <aaroongeorge2005@gmail.com>
2024-09-22 07:50:32 +0000

    completed contacts

---
+++ components/Contacts/ContactCard.jsx
@@ -0,0 +1,20 @@
+import "./Contacts.css"
+
+const ContactCard = ({ name, email, phone }) => {
+    return (
+        <div className="custom-border p-1 rounded-lg w-[300px] h-[400px]">
+            <div className="bg-[#27272A] rounded-lg w-full h-full flex flex-col justify-around items-center">
+                <div />
+                <img src="https://via.placeholder.com/150" alt="Contact" className="w-40 h-40 rounded-full mb-2" />
+                <div className="flex flex-col items-center justify-center">
+                    <h3 className="text-white text-xl font-bold mb-2">{name}</h3>
+                    <p className="text-white mb-2">{email}</p>
+                    <p className="text-white">{phone}</p>
+                </div>
+                <div />
+            </div>
+        </div>
+    );
+};
+
+export default ContactCard;
---
+++ components/Contacts/Contacts.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ContactCard from './ContactCard';
+
+const Contacts = () => {
+  const contactData = [
+    { name: 'John Doe', email: 'john.doe@example.com', phone: '123-456-7890' },
+    { name: 'Jane Smith', email: 'jane.smith@example.com', phone: '987-654-3210' }
+  ];
+
+  return (
+    <div id="contacts" className='flex justify-around mb-20 flex-wrap gap-20 mx-40'>
+      {contactData.map((contact, index) => (
+        <ContactCard key={index} name={contact.name} email={contact.email} phone={contact.phone} />
+      ))}
+    </div>
+  );
+};
+
+export default Contacts;
