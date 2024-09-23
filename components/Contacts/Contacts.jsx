516250ad6cd3ef7a41f4ed4fdf3beda621a1a1bd
1337kid <45532566+1337kid@users.noreply.github.com>
2024-09-23 07:39:12 +0530

    update contacts

--- components/Contacts/Contacts.jsx
+++ components/Contacts/Contacts.jsx
@@ -4,16 +4,19 @@ import { contactData } from "@/constants";
 
 const Contacts = () => {
   return (
-    <div id="contacts" className='flex justify-around mb-20 flex-wrap gap-20 sm:mx-40'>
-      {contactData.map((contact, index) => (
-        <ContactCard 
-          key={index} 
-          name={contact.name} 
-          title={contact.title} 
-          phone={contact.phone} 
-          image={contact.image}  
-        />
-      ))}
+    <div id="contacts" className='flex my-20 flex-wrap gap-20 sm:mx-40 flex-col items-center justify-center'>
+      <h4 className='text-3xl md:text-5xl text-center text-gray-light w-full font-semibold'>Contact<span className='text-red'>US</span></h4>
+      <div className='flex justify-around flex-wrap gap-20'>
+        {contactData.map((contact, index) => (
+          <ContactCard 
+            key={index} 
+            name={contact.name} 
+            title={contact.title} 
+            phone={contact.phone} 
+            image={contact.image}  
+          />
+        ))}
+      </div>
     </div>
   );
 };
