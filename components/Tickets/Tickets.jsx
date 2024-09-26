ff692c139611ac01c0c1548246df8de9ab0d10ef
aaron-6960 <138325645+aaron-6960@users.noreply.github.com>
2024-09-27 01:58:44 +0530

    Tickets done

--- components/Tickets/Tickets.jsx
+++ components/Tickets/Tickets.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Image from "next/image";
+import Ticket from "@/assets/Ticket.png"
 
 function Tickets() {
   return (
     <div className="max-w-screen-xl flex flex-col px-4 py-8 mx-auto lg:gap-8 justify centre items-center">
       <h1 className="text-4xl font-bold text-center mb-20">Tickets</h1>
-      <div
+      {/*<div
         className="relative rounded-full bg-red/[.1] md:my-[40px] p-4 text-left border-[2px] border-red flex flex-col overflow-hidden w-[60vw] min-w-[398px] justify-center items-center py-20"
         data-aos="fade-up"
       >
@@ -12,7 +14,8 @@ function Tickets() {
         <h2 className="text-[2rem]">Total Tickets : 80</h2>
         <h2 className="text-[2rem]">Student Ticket: 199₹</h2>
         <h2 className="text-[2rem]">Open Ticket: 299₹</h2>
-      </div>
+      </div>*/}
+      <Image src={Ticket} alt="true" className="" />
     </div>
   );
 }
